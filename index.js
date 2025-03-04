24c9e3ec612a26b4fb6351f74e6a6bae52e61435
Duncan Kariuki <duncankariukimugo@gmail.com>
2025-03-04 13:28:11 +0300

    adds index.js

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code in this file!
+const currentUser="Duncan Kariuki!";
+const welcomeMessage="Welcome to Flatbook, "+currentUser;
+const excitedWelcomeMessage= welcomeMessage.toUpperCase()+currentUser;
+const shortGreeting="Welcome, "+currentUser[0]+"!";
